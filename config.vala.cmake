e2d79e66a65528653df27b0fc6e44cf1cdd2976f
Michael P. Starkweather <mpstark@gmail.com>
2014-08-19 23:59:44 -0500

    Updated to newest CMake tools.

--- config.vala.cmake
+++ config.vala.cmake
@@ -1,5 +1,5 @@
-namespace Constants {
-public const string PLUGNAME = "@PLUGNAME@";
+namespace Build {
+public const string PLUGCODENAME = "@CMAKE_PROJECT_NAME@";
 public const string DATADIR = "@DATADIR@";
 public const string PKGDATADIR = "@PKGDATADIR@";
 public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
