0fab7789cb3300bca4d27b89fe22da9fed550b4a
Ryo Nakano <ryonakaknock3@gmail.com>
2025-11-29 11:52:20 +0900

    chore: Apply shellcheck (#339)

--- build-aux/start-pantheon-tweaks.sh
+++ build-aux/start-pantheon-tweaks.sh
@@ -7,15 +7,17 @@ trap "finalize; exit 1" SIGINT
 # Cleanup temporary symlinks and directories this script created.
 finalize()
 {
-  for dir in "${HOST_XDG_DATA_DIRS//:/ }"; do
-    unlink $dir/glib-2.0/schemas
-    rmdir $dir/glib-2.0
-    rmdir $dir
+  for dir in ${HOST_XDG_DATA_DIRS//:/ }; do
+    unlink "$dir/glib-2.0/schemas"
+    rmdir "$dir/glib-2.0"
+    rmdir "$dir"
   done
 
   rmdir "$bridge_dir"
 }
 
+# $XDG_DATA_DIRS is meant to be expanded in the sh launched by flatpak-spawn instead of this script
+# shellcheck disable=SC2016
 IFS=: read -ra host_data_dirs < <(flatpak-spawn --host sh -c 'echo "$XDG_DATA_DIRS"')
 
 # To avoid potentially muddying up $XDG_DATA_DIRS too much, we link the schema paths
@@ -42,7 +44,7 @@ done
 # We MUST prepend the host's data dirs BEFORE the Flatpak environment's own dirs,
 # otherwise data (such as default values) load in the wrong order and would then
 # incorrectly prefer the Flatpak's internal defaults instead of the host's defaults!
-if [[ ! -z "${HOST_XDG_DATA_DIRS}" ]]; then
+if [[ -n "${HOST_XDG_DATA_DIRS}" ]]; then
   XDG_DATA_DIRS="${HOST_XDG_DATA_DIRS:1}:${XDG_DATA_DIRS}"
 fi
 
